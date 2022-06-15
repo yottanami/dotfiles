a7019fa9e5d3cb08cc553e8aa91042cee807c1d4
Behnam Khanbeigi <b.khanbeigi@gresb.com>
2022-06-15 20:55:49 +0200

    Initial

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set PATH /home/yottanami/.local/bin/ $PATH
+set PATH /home/yottanami/.rbenv/bin/ $PATH
+set PATH /home/yottanami/.yarn/bin $PATH
+set PATH /home/yottanami//bin/ $PATH
+set PATH /home/yottanami/.platformio/penv/bin/ $PATH
+set fish_greeting
+set FG42_V3 true
+alias g="git"
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+status --is-interactive; and source (rbenv init -|psub)
+eval (ssh-agent -c)
